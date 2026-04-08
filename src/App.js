bd867a22b9bfb65884fe62498e2366119b67ac17
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-08 14:48:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to development code for react!
+          Welcome to application development code for react!
         </p>
         <a
           className="App-link"
