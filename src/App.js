14774793f54d807bce43f63df253a0fd5d569b46
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-10 12:08:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome G3 innovation pvtltd!
+          Welcome G3 innovation pvt ltd!
         </p>
         <a
           className="App-link"
