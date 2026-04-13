b9345c1eb4bf8f20fadc221695f972d39ecfb264
Maddala rajesh <112849261+Maddalarajesh@users.noreply.github.com>
2026-04-13 16:14:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing the react code!
+          testing the frontend react code!
         </p>
         <a
           className="App-link"
